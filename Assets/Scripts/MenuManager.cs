f06a95032296ca3d5f3c380a0f8b4bfa164c3574
Harvester <53939102+HarvesterDeveloper@users.noreply.github.com>
2023-09-22 17:07:19 +0300

    Main menu parallax

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,8 +1,18 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject background;
+    private float lifeTime = 0f;
+
+    private void Update()
+    {
+        lifeTime += Time.deltaTime;
+
+        background.transform.position = new Vector2(Mathf.Cos(lifeTime) * 20f, Mathf.Sin(lifeTime) * 10f);
+    }
+
     public void LaunchGame()
     {
         SceneManager.LoadScene("Match"); // Change it to right name
