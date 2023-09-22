97be4161f55911dbbb33d6d45d3423b9fef049c7
Harvester <53939102+HarvesterDeveloper@users.noreply.github.com>
2023-09-22 20:31:30 +0300

    Fade in match

--- Assets/Scripts/HudManager.cs
+++ Assets/Scripts/HudManager.cs
@@ -9,9 +9,32 @@ public class HudManager : MonoBehaviour
     [SerializeField] private Image healthImage;
     [SerializeField] private Image progressImage;
     [SerializeField] private TMP_Text progressText;
+    [SerializeField] private Image fader;
+    private bool fadeLimiter = false;
+    private float fadePower = 0.1f;
+    private float timeSinceStart = 0f;
 
     private void Update()
     {
+        timeSinceStart += Time.deltaTime;
+        
+        if (!fadeLimiter)
+        {
+            if (timeSinceStart < 2.5f)
+            {
+                Color temp = fader.color;
+                temp.a -= fadePower * Time.deltaTime;
+                fader.color = temp;
+                fadePower += 0.25f * Time.deltaTime;
+            }
+            else
+            {
+                Destroy(fader.gameObject);
+                fadeLimiter = true;
+            }
+        }
+
+
         healthImage.fillAmount = gameManager.Player.Health / gameManager.Player.MaxHealth;
         progressImage.fillAmount = (float) gameManager.Player.KillCount / (float) gameManager.RequiredToLevelUp;
         progressText.text = "Прогресс: " + gameManager.Player.KillCount + "/" + gameManager.RequiredToLevelUp
