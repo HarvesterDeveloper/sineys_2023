ac7522a5d34484efa16b70d128cd3db82b95ee35
Harvester <53939102+HarvesterDeveloper@users.noreply.github.com>
2023-09-22 13:14:03 +0300

    Progress bar in UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,30 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private PlayerController playerController;
+    private int enemiesDied = 0;
+    [SerializeField] private int requiredToKill = 10;
 
     public PlayerController Player
     {
         get
         {
             return playerController;
+        }
+    }
+
+    public int EnemiesDied
+    {
+        get
+        {
+            return enemiesDied;
+        }
+    }
+
+    public int RequiredToKill
+    {
+        get
+        {
+            return requiredToKill;
         }
     }
 
--- Assets/Scripts/HudManager.cs
+++ Assets/Scripts/HudManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,9 +7,12 @@ public class HudManager : MonoBehaviour
     [SerializeField] private GameManager gameManager;
     [Header("HUD Elements")]
     [SerializeField] private Image healthImage;
+    [SerializeField] private Image progressImage;
+    [SerializeField] private TMP_Text progressText;
 
     private void Update()
     {
         healthImage.fillAmount = gameManager.Player.Health / gameManager.Player.MaxHealth;
+        progressImage.fillAmount = gameManager.EnemiesDied / gameManager.RequiredToKill;
     }
 }
